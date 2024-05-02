b1178d64ecfd30c7f8146d0dc9457277ba8c7a74
usama387 <128306049+usama387@users.noreply.github.com>
2024-05-02 18:44:42 +0500

    Integrated prisma and mongodb and added my all auth routes

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 // creating the app and server listening port
 import express from "express";
+import cookieParser from "cookie-parser";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/posts", postRoute);
 
 app.use("/api/auth", authRoute);
 
-app.listen(8900, () => {
-  console.log("The app server is running");
+app.listen(8700, () => {
+  console.log("The app server is running on 8700");
 });
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,18 +1,88 @@
+import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
+import jwt from "jsonwebtoken";
+
 // first controller for registeration
-export const register = (req, res) => {
+export const register = async (req, res) => {
+  // destructuring items from request body
   const { username, email, password } = req.body;
 
-  //hash the password
-  //25:11
-  //create the new user
+  try {
+    // hashing password with bcrypt method in 10 digits
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // creating new user with prisma and mongodb
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    // sending response to client
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status((500).json({ message: "Failed to create user!" }));
+  }
 };
 
-// first controller for login
-export const login = (req, res) => {
-  // db operations
+// second controller for login and cookie json web token authentication
+export const login = async (req, res) => {
+  // destructuring items from request body
+  const { username, password } = req.body;
+
+  try {
+    // finding user with prisma and mongodb using username
+    const user = await prisma.user.findUnique({
+      where: {
+        username,
+      },
+    });
+
+    // when user not found
+    if (!user) {
+      res.status(401).json({ message: "Invalid Credentials!" });
+    }
+
+    // comparing password and sending response
+    const isPasswordValid = bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      res.status(401).json({ message: "Password does not match!" });
+    }
+
+    // not a good way to get cookies
+    // res.setHeader("Set-Cookie", "test=" + "myValue").json("success");
+
+    // cookies expire timing approx 1 week
+    const age = 1000 * 60 * 60 * 24 * 7;
+
+    // creating a json web token for user
+    const token = jwt.sign(
+      {
+        id: user.id,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: age }
+    );
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        // secure: true,
+        maxAge: age,
+      })
+      .status(200)
+      .json({ message: "Login successful" });
+  } catch (error) {
+    console.log(error);
+    res.status((500).json({ message: "Failed to login!" }));
+  }
 };
 
 // first controller for logout
 export const logout = (req, res) => {
-  // db operations
+  res.clearCookie("token").status(200).json({ message: "Logout Sucessfull" });
 };
