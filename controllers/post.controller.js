6c7294983da5bc6b164dd2f988a10abdc25bb51a
usama387 <128306049+usama387@users.noreply.github.com>
2024-05-21 17:36:55 +0500

    Added Search bar configuration with this api to fetch desired posts

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -2,8 +2,24 @@ import prisma from "../lib/prisma.js";
 
 // first controller to get all posts
 export const getPosts = async (req, res) => {
+  // this variable is being used to get query and pass results in listPage loader to fetch desired posts
+  const query = req.query;
+
   try {
-    const posts = await prisma.post.findMany();
+    const posts = await prisma.post.findMany({
+      where: {
+        city: query.city || undefined,
+        type: query.type || undefined,
+        property: query.property || undefined,
+
+        // since bedroom no is an integer
+        bedroom: parseInt(query.bedroom) || undefined,
+        price: {
+          gte: parseInt(query.minPrice) || 0,
+          lte: parseInt(query.maxPrice) || 10000000,
+        },
+      },
+    });
 
     res.status(200).json(posts);
   } catch (error) {
